a134de74c7befa170c83d55ce765ad1876a819e8
Mwkos Panagiotis <mwkospanagiotis@gmail.com>
2025-10-16 06:29:39 +0300

    Add Head component with SEO meta tags
    This component includes meta tags for SEO and a title for the webpage.

---
+++ app/head.tsx
@@ -0,0 +1,20 @@
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="google-site-verification"
+        content="XLcExvzTzyf3PrfNEG06B7zSxJljnGjrQNlUZUuHUus"
+      />
+      <title>Panagiotis Mokos | AI & Web Developer</title>
+      <meta
+        name="description"
+        content="Explore the portfolio of Panagiotis Mokos — AI and Web Developer creating modern, fast, and elegant digital experiences."
+      />
+      <meta
+        name="keywords"
+        content="Panagiotis Mokos, AI Developer, Web Developer, Next.js, React, Portfolio"
+      />
+      <meta name="author" content="Panagiotis Mokos" />
+    </>
+  );
+}
